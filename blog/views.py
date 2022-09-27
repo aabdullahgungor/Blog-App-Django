2cd1e0010f5171c525e0d5f6d9ab67bd7b54dc94
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2022-09-27 17:19:30 +0300

    Dynamic Data Added

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,46 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render 
 
+data = {
+    "blogs": [
+        {
+            "id": 1,
+            "title": "Komple Web Geliştirme",
+            "image": "1.jpg",
+            "is_active": True,
+            "is_home": True, # For homepage
+            "description": "çok iyi bir kurs"
+        },
+        {
+            "id": 2,
+            "title": "Python Kursu",
+            "image": "2.jpg",
+            "is_active": True,
+            "is_home": False,
+            "description": "çok iyi bir kurs"
+        },
+        {
+            "id": 3,
+            "title": "Django Kursu",
+            "image": "3.jpg",
+            "is_active": False,
+            "is_home": True,
+            "description": "çok iyi bir kurs"
+        }
+    ]
+}
+
 def index(request):
-    return render(request, "blog/index.html")
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request, "blog/index.html", context)
 
 def blogs(request):
-    return render(request, "blog/blogs.html")
+    context = {
+        "blogs": data["blogs"]
+    }
+    return render(request, "blog/blogs.html",context)
 
 def blog_details(request, id):
     return render(request, "blog/blog_details.html",{
