46e17445ca5816adca7a3a687acdcaece6d8e305
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2022-10-02 16:42:18 +0300

    Many To One Relationship Defined

--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,9 @@
+from unicodedata import category
 from django.db import models
 from django.utils.text import slugify
 from ckeditor.fields import RichTextField
 
 # Create your models here.
-
-class Blog(models.Model):
-    title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to="blogs")
-    description = RichTextField()
-    is_active = models.BooleanField(default=False)
-    is_home = models.BooleanField(default=False)
-    slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
-
-    def __str__(self):
-        return f"{self.title}"
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
 
 class Category(models.Model):
     name = models.CharField(max_length=150)
@@ -28,4 +14,20 @@ class Category(models.Model):
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="blogs")
+    description = RichTextField()
+    is_active = models.BooleanField(default=False)
+    is_home = models.BooleanField(default=False)
+    slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
+    category = models.ForeignKey(Category,default=1, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
         super().save(*args, **kwargs)
