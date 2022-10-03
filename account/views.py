6f4a1c2866ade2fe97c711c628ca0f966c7db50f
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2022-10-03 19:14:42 +0300

    User Register Added

--- account/views.py
+++ account/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login
-
-# Create your views here.
+from django.contrib.auth.models import User
 
 def login_request(request):
     if request.method == "POST":
@@ -21,6 +20,47 @@ def login_request(request):
     return render(request, "account/login.html")
 
 def register_request(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        firstname = request.POST["firstname"]
+        lastname = request.POST["lastname"]
+        password = request.POST["password"]
+        repassword = request.POST["repassword"]
+
+        if password == repassword:
+            if User.objects.filter(username=username).exists():
+                return render(request, "account/register.html", 
+                {
+                    "error":"username kullanılıyor.",
+                    "username":username,
+                    "email":email,
+                    "firstname": firstname,
+                    "lastname":lastname
+                })
+            else:
+                if User.objects.filter(email=email).exists():
+                    return render(request, "account/register.html", 
+                    {
+                        "error":"email kullanılıyor.",
+                        "username":username,
+                        "email":email,
+                        "firstname": firstname,
+                        "lastname":lastname
+                    })
+                else:
+                    user = User.objects.create_user(username=username,email=email,first_name=firstname,last_name=lastname,password=password)
+                    user.save()
+                    return redirect("login")                    
+        else:
+            return render(request, "account/register.html", {
+                "error":"parola eşleşmiyor.",
+                "username":username,
+                "email":email,
+                "firstname": firstname,
+                "lastname":lastname
+            })
+
     return render(request, "account/register.html")
 
 def logout_request(request):
