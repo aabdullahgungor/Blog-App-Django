9d9caf9a9fd55b7e1238b28daeafdadda0d86fd8
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2022-10-03 17:07:36 +0300

    User Login Added

--- account/views.py
+++ account/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def login_request(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            return render(request, "account/login.html", {
+                "error": "username ya da parola yanlış"
+            })
+
     return render(request, "account/login.html")
 
 def register_request(request):
